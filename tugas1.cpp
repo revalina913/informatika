13e07b774c43934537daf4f4d82e5774aa632a47
revalina913 <revalina142010@gmail.com>
2025-08-05 14:28:40 +0700

    Add files via upload

---
+++ tugas1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "revalina " <<endl;
+    cout << "Cianjur selagedang " <<endl;
+    cout << "14 04 2010 " <<endl;
+    
+    }
+    
+    
