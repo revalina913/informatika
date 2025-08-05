3d3fb46082092af3364a7a722ef27a6331d91503
revalina913 <revalina142010@gmail.com>
2025-08-05 14:29:35 +0700

    Add files via upload

---
+++ aplikasi penjumlahan sederhan.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "aplikasi penjumlahan\n;";
+    cout << "masukan angka pertama:";
+    int bil1,bil2,hasil;
+    cin >> bil1;
+    cout << "masukan angka kedua:";
+    cin >> bil2;
+    hasil = bil1+bil2;
+    cout << hasil;
+    
+    }
+    
+    
